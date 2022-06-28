3c785579eed900b5b9c3842f34989b105b215a0f
TinhLD <tinhtobe@gmail.com>
2022-06-28 09:55:25 +0700

    Init

---
+++ Assets/A14Building/Scripts/A14Building/Buildings/Building.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A14Building.Build
+{
+    public class Building : MonoBehaviour
+    {
+        
+    }
+}
+
